1c962e9a9f5d8f6856b6f690741570b648caf050
abhishekdevinfo <abhi.info.dev@gmail.com>
2017-03-23 23:30:07 +0530

    Apply JSX

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,18 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
-      div(null,
-        MyTitleFactory({ title: 'first prop', color: 'indianred' }),
-        MyTitleFactory({ title: '2222 prop', color: 'tomato' }),
-        MyTitleFactory({ title: 'THird prop', color: 'mediumorchid' })
-      )
+      <div>
+        <MyTitle title='props are cool' color='rebeccapurple' />
+        <MyTitle title='Boom Boom' color='tomato' />
+        <MyTitle title='props are cool' color='mediumaquamarine' />        
+      </div>
     )
   }
 })
