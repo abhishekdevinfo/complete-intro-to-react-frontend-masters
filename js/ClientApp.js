1ac724526e2b8e38e5af0ebb5377fc3bac793797
abhishekdevinfo <abhi.info.dev@gmail.com>
2017-03-25 15:17:15 +0530

    Match is changed to matchPath, HashRouter and matchPath is now available in react-router-dom

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { HashRouter, matchPath } from 'react-router-dom'
+import Landing from './Landing'
 import '../public/normalize.css'
 import '../public/style.css'
 
 const App = React.createClass({
   render () {
     return (
-      <div className='app'>
-        <div className='landing'>
-          <h1>Video Store</h1>
-          <input type='text' placeholder='Search' />
-          <a>Browse All</a>
+      <HashRouter>
+        <div className='app'>
+          <matchPath exactly pattern='/' component={Landing} />
         </div>
-      </div>
+      </HashRouter>
     )
   }
 })
